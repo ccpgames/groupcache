0f22bc61ff717315bb7b733ac08074c48c983443
Mateusz Krasucki <mateusz@ccpgames.com>
2026-05-29 16:23:30 +0000

    feat: better peer management

--- http_test.go
+++ http_test.go
@@ -28,6 +28,7 @@ import (
 	"net/http/httptest"
 	"os"
 	"os/exec"
+	"slices"
 	"strconv"
 	"strings"
 	"sync"
@@ -95,7 +96,8 @@ func TestHTTPPool(t *testing.T) {
 	wg.Wait()
 
 	// Use a dummy self address so that we don't handle gets in-process.
-	p := NewHTTPPool("should-be-ignored")
+	self := "should-be-ignored"
+	p := NewHTTPPool(self)
 	p.Set(addrToURL(childAddr)...)
 
 	// Dummy getter function. Gets should normally go to children only; the
@@ -214,6 +216,7 @@ func TestHTTPPool(t *testing.T) {
 	err = g.Get(ctx, "IReturnInternalError", StringSink(&value))
 	assert.Equal(t, "I am a errors.New() error", err.Error())
 
+	testHTTPPoolSet(t, p, self)
 }
 
 func testKeys(n int) (keys []string) {
@@ -295,6 +298,124 @@ func awaitAddrReady(t *testing.T, addr string, wg *sync.WaitGroup) {
 	}
 }
 
+func testHTTPPoolSet(t *testing.T, httpPool *HTTPPool, self string) {
+	t.Helper()
+	peers := []string{"http://peer1:8080", "http://peer2:8080"}
+	peersIncludingSelf := append(peers, self)
+
+	// set on the pool
+	httpPool.Set(peersIncludingSelf...)
+
+	// check if returned peer list is correct
+	returnedPeers := httpPool.GetPeers()
+	t.Logf("Returned peers: %v", returnedPeers)
+	if len(returnedPeers) != len(peersIncludingSelf) {
+		t.Fatalf("expected %d peers, got %d", len(peersIncludingSelf), len(returnedPeers))
+	}
+	if !slices.Equal(peersIncludingSelf, returnedPeers) {
+		t.Fatalf("expected peers %v, got %v", peersIncludingSelf, returnedPeers)
+	}
+
+	// all my getters should be for peers
+	peerGetters := httpPool.GetAll()
+	for _, getter := range peerGetters {
+		t.Logf("Getter peer returned: %s", getter.GetURL())
+		httpGetter, ok := getter.(*httpGetter)
+		if !ok {
+			t.Fatalf("expected getter to be of type *httpGetter, got %T", getter)
+		}
+		peer := httpGetter.GetPeer()
+		if !slices.Contains(peers, peer) {
+			t.Fatalf("expected getter URL %s to be in peers list %v", peer, peers)
+		}
+	}
+
+	// now I will set again with peer1 removed, and peer3 added
+	newPeers := []string{"http://peer2:8080", "http://peer3:8080"}
+	newPeersIncludingSelf := append(newPeers, self)
+	httpPool.Set(newPeersIncludingSelf...)
+
+	// check if returned peer list is correct
+	returnedPeers = httpPool.GetPeers()
+	t.Logf("Returned peers after update: %v", returnedPeers)
+	if len(returnedPeers) != len(newPeersIncludingSelf) {
+		t.Fatalf("expected %d peers, got %d", len(newPeersIncludingSelf), len(returnedPeers))
+	}
+	if !slices.Equal(newPeersIncludingSelf, returnedPeers) {
+		t.Fatalf("expected peers %v, got %v", newPeersIncludingSelf, returnedPeers)
+	}
+
+	// all  getters should be for peers
+	newPeerGetters := httpPool.GetAll()
+	for _, getter := range newPeerGetters {
+		t.Logf("Getter peer returned after update: %s", getter.GetURL())
+		httpGetter, ok := getter.(*httpGetter)
+		if !ok {
+			t.Fatalf("expected getter to be of type *httpGetter, got %T", getter)
+		}
+		peer := httpGetter.GetPeer()
+		if !slices.Contains(newPeers, peer) {
+			t.Fatalf("expected getter URL %s to be in peers list %v", peer, newPeers)
+		}
+	}
+
+	// the peer1 getter should be closed and should not be in the new getters list
+	var peer1GetterBefore, peer1GetterAfter *httpGetter
+	for _, getter := range peerGetters {
+		httpGetter := getter.(*httpGetter)
+		if httpGetter.GetPeer() == "http://peer1:8080" {
+			peer1GetterBefore = httpGetter
+			select {
+			case <-peer1GetterBefore.peerLifetimeCtx.Done():
+			default:
+				t.Fatal("expected peer1 getter to be closed, but it was not")
+			}
+			break
+		}
+	}
+	for _, getter := range newPeerGetters {
+		httpGetter := getter.(*httpGetter)
+		if httpGetter.GetPeer() == "http://peer1:8080" {
+			peer1GetterAfter = httpGetter
+			break
+		}
+	}
+	if peer1GetterBefore == nil {
+		t.Fatal("expected to find peer1 getter in old getters")
+	}
+	if peer1GetterAfter != nil {
+		t.Fatal("expected to not find peer1 getter in new getters")
+	}
+
+	// the peer2 getter should not be closed and should be the same instance as before
+	var peer2GetterBefore, peer2GetterAfter *httpGetter
+	for _, getter := range peerGetters {
+		httpGetter := getter.(*httpGetter)
+		if httpGetter.GetPeer() == "http://peer2:8080" {
+			peer2GetterBefore = httpGetter
+			select {
+			case <-peer2GetterBefore.peerLifetimeCtx.Done():
+				t.Fatal("expected peer2 getter to not be closed, but it was")
+			default:
+			}
+			break
+		}
+	}
+	for _, getter := range newPeerGetters {
+		httpGetter := getter.(*httpGetter)
+		if httpGetter.GetPeer() == "http://peer2:8080" {
+			peer2GetterAfter = httpGetter
+			break
+		}
+	}
+	if peer2GetterBefore == nil || peer2GetterAfter == nil {
+		t.Fatal("expected to find peer2 getter in both old and new getters")
+	}
+	if peer2GetterBefore != peer2GetterAfter {
+		t.Fatal("expected peer2 getter instance to be the same after Set, but it was not")
+	}
+}
+
 func TestHttpGetterClosed(t *testing.T) {
 	// save & restore process-globals so this test composes with others
 	// (TestHTTPPool runs first and registers its own picker; calling
@@ -312,6 +433,7 @@ func TestHttpGetterClosed(t *testing.T) {
 	defer cancelGetter1()
 	httpGetter1 := &httpGetter{
 		baseURL:            "http://does-not.exist",
+		peer:               "http://does-not.exist",
 		peerLifetimeCtx:    getter1Ctx,
 		peerLifetimeCancel: cancelGetter1,
 	}
@@ -319,6 +441,7 @@ func TestHttpGetterClosed(t *testing.T) {
 	defer cancelGetter2()
 	httpGetter2 := &httpGetter{
 		baseURL:            "http://this-too-does-not.exist",
+		peer:               "http://this-too-does-not.exist",
 		peerLifetimeCtx:    getter2Ctx,
 		peerLifetimeCancel: cancelGetter2,
 	}
@@ -365,7 +488,6 @@ func TestHttpGetterClosed(t *testing.T) {
 		// this is unexpected
 		t.Fatal("expected Get to return after peerLifetimeCtx was canceled")
 	}
-
 }
 
 type testPeerPicker struct {
